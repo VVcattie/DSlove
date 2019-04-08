5b425a5de356a0bd340e3a7f90312cdbd1a5fbcb
VVcattie <48469990+VVcattie@users.noreply.github.com>
2019-04-08 15:24:29 -0600

    creating the new file
    Testing this thing

---
+++ test.cpp
@@ -0,0 +1,20 @@
+//to insert data in BST, returns address of root node
+BstNode* Insert (BstNode* root, int data)
+{
+  if (root ==NULL)
+  {//empty tree
+    root = GetNewNode(data);
+  }
+  //if data to be inserted is lesser, insert in left subtree
+  else if(data<root->data)
+  {
+    root->left = Insert (root->right, data);
+  }
+  else
+  {
+    root->right = Insert(root->right, data);
+  }
+  return root;
+}
+/*
+// https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet
